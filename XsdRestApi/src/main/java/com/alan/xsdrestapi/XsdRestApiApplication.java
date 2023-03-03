8ced5badefdf322f93a9e0b3ce9e7f17afa69ad6
dautovicalan <dautovic2000@gmail.com>
2023-03-03 18:13:22 +0100

    config files and properiets

---
+++ XsdRestApi/src/main/java/com/alan/xsdrestapi/XsdRestApiApplication.java
@@ -0,0 +1,11 @@
+package com.alan.xsdrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsdRestApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XsdRestApiApplication.class, args);
+    }
+}
